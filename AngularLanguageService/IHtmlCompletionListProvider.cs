604506097857d149afea5f54dcd7b4346dcacdf3
Shennie Yao <sheya@microsoft.com>
2021-01-12 16:04:59 -0500

    adding HTML completion to angular language service

---
+++ AngularLanguageService/IHtmlCompletionListProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.WebTools.Languages.Html.Editor.Completion;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace AngularLanguageService.Html.Completion
+{
+    /// <summary>
+    /// An interface that supplies list of entries to intellisense.
+    /// There may be more than one provider.
+    /// Providers are exported via MEF, <see cref="HtmlCompletionProviderAttribute"/>
+    /// </summary>
+    public interface IHtmlCompletionListProvider
+    {
+        /// <summary>
+        /// Retrieves list of intellisense entries
+        /// </summary>
+        /// <param name="context">Completion context</param>
+        /// <returns>List of completion entries</returns>
+        IList<HtmlCompletion> GetEntries(HtmlCompletionContext context);
+    }
+}
