d9cd4ae04783bea6b602a8d38425577b0e0a8b98
Maria Solano <mariasolano@microsoft.com>
2022-10-27 14:49:07 -0700

    Separate component type defs

---
+++ AngularLanguageService.Shared/LanguageServer/AngularComponentContentTypeDefinitions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Shared.ContentTypes;
+
+namespace AngularLanguageService.Shared.LanguageServer
+{
+    internal static class AngularComponentContentTypeDefinitions
+    {
+#pragma warning disable CS0649 // Field 'field' is never assigned to, and will always have its default value null.
+        [Export]
+        [Name(AngularConstants.AngularComponentContentTypeName)]
+        [BaseDefinition(HtmlContentTypeDefinition.HtmlContentType)]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition AngularComponentContentType;
+
+        [Export]
+        [Name(AngularConstants.AngularComponentDelegationContentTypeName)]
+        [BaseDefinition(HtmlContentTypeDefinition.HtmlDelegationContentType)]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition AngularComponentDelegationContentType;
+    }
+}
---
+++ AngularLanguageService.Shared/LanguageServer/AngularConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularLanguageService.Shared.LanguageServer
+{
+    internal static class AngularConstants
+    {
+        internal const string TypeScriptContentTypeName = "TypeScript";
+
+        internal const string AngularComponentContentTypeName = "angularcomponent";
+        internal const string AngularComponentDelegationContentTypeName = "angularcomponent-delegation";
+        internal const string AngularComponentContentTypeExtension = ".component.html";
+    }
+}
