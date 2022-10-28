49b97e1e0fa940f57e6ecbf4c19974daf544b0bf
Maria Solano <mariasolano@microsoft.com>
2022-10-28 16:34:12 -0700

    Define decorator middle layer

---
+++ AngularLanguageService.Shared/LanguageServer/AggregatingMiddleLayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Newtonsoft.Json.Linq;
+
+namespace AngularLanguageService.Shared.LanguageServer
+{
+    /// <summary>
+    /// Composite <see href="https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.languageserver.client.ilanguageclientmiddlelayer?view=visualstudiosdk-2022">
+    /// middle layer</see> which aggregates all concrete instances of <see cref="MiddleLayerHandler"/> and 
+    /// accordingly delegates the interception to the aggregated layers.
+    /// </summary>
+    /// <remarks>
+    /// Because a single handler will be invoked for each request or notification, assumes that there's at most 
+    /// one middle layer handling any given method.
+    /// </remarks>
+    [Export]
+    internal sealed class AggregatingMiddleLayer : ILanguageClientMiddleLayer
+    {
+        private readonly IEnumerable<MiddleLayerHandler> middleLayerHandlers;
+
+        [ImportingConstructor]
+        public AggregatingMiddleLayer([ImportMany] IEnumerable<MiddleLayerHandler> middleLayerHandlers)
+        {
+            this.middleLayerHandlers = middleLayerHandlers;
+        }
+
+        bool ILanguageClientMiddleLayer.CanHandle(string methodName) => this.middleLayerHandlers.Any(handler => handler.CanHandle(methodName));
+
+        Task ILanguageClientMiddleLayer.HandleNotificationAsync(string methodName, JToken methodParam, Func<JToken, Task> sendNotification)
+            => this.GetHandler(methodName).HandleNotificationAsync(methodName, methodParam, sendNotification);
+
+        Task<JToken> ILanguageClientMiddleLayer.HandleRequestAsync(string methodName, JToken methodParam, Func<JToken, Task<JToken>> sendRequest)
+            => this.GetHandler(methodName).HandleRequestAsync(methodName, methodParam, sendRequest);
+
+        private ILanguageClientMiddleLayer GetHandler(string methodName) => this.middleLayerHandlers.First(layer => layer.CanHandle(methodName));
+    }
+}
---
+++ AngularLanguageService.Shared/LanguageServer/MiddleLayerHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Newtonsoft.Json.Linq;
+
+namespace AngularLanguageService.Shared.LanguageServer
+{
+    /// <summary>
+    /// Base class for an <see cref="ILanguageClientMiddleLayer"/> of the Angular LSP service.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="AggregatingMiddleLayer"/> delegates the concrete middle layer handlers and delegates the respective 
+    /// requests/notifications to the correct handler.
+    /// </remarks>
+    internal abstract class MiddleLayerHandler : ILanguageClientMiddleLayer
+    {
+        private readonly HashSet<string> methodsToHandle;
+
+        protected MiddleLayerHandler(params string[] methodsToHandle)
+        {
+            this.methodsToHandle = new HashSet<string>(methodsToHandle);
+        }
+
+        public bool CanHandle(string methodName) => this.methodsToHandle.Contains(methodName);
+
+        public virtual Task HandleNotificationAsync(string methodName, JToken methodParam, Func<JToken, Task> sendNotification) => sendNotification(methodParam);
+
+        public virtual Task<JToken> HandleRequestAsync(string methodName, JToken methodParam, Func<JToken, Task<JToken>> sendRequest) => sendRequest(methodParam);
+    }
+}
+
