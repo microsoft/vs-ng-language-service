ba5c7f1c82aa87d5907c441fdc3652ac9da3e0b2
Maria Solano <mariasolano@microsoft.com>
2022-10-31 13:16:03 -0700

    Format diagnostic messages

--- AngularLanguageService.2022/LanguageServer/DiagnosticFormatterMiddleLayer.cs
+++ AngularLanguageService.2022/LanguageServer/DiagnosticFormatterMiddleLayer.cs
@@ -18,8 +18,23 @@ namespace AngularLanguageService.LanguageServer
 
         public override Task HandleNotificationAsync(string methodName, JToken methodParam, Func<JToken, Task> sendNotification)
         {
-            // TODO: Format diagnostics (see #47).
-            return base.HandleNotificationAsync(methodName, methodParam, sendNotification);
+            var diagnosticParams = methodParam.ToObject<PublishDiagnosticParams>();
+
+            foreach (Diagnostic diagnostic in diagnosticParams.Diagnostics)
+            {
+                // Add an Angular code prefix
+                diagnostic.Code = "(NG) " + diagnostic.Code?.Value;
+
+                // The Angular parser adds location information to errors (see https://github.com/angular/angular/blob/37ba6104498202b671f5a5a6bbfacc4df501788b/packages/compiler/src/expression_parser/ast.ts#L12-L18),
+                // resulting in redundant messages in the error list.
+                if (diagnostic.Message.StartsWith("Parser Error: "))
+                {
+                    int endOfMessage = diagnostic.Message.LastIndexOf("] in ");
+                    diagnostic.Message = diagnostic.Message.Substring(0, endOfMessage >= 0 ? endOfMessage + 1 : diagnostic.Message.Length);
+                }
+            }
+
+            return sendNotification(JToken.FromObject(diagnosticParams));
         }
     }
 }
